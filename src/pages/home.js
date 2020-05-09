56aab750a124c4fedcede85f6b1ec701c3836a4d
Danny Tran <danny.b.tran@disney.com>
2020-05-09 08:22:35 -0500

    misc: more ux

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,18 +2,22 @@ import m from 'mithril'
 import x from 'aes256'
 
 export default () => {
-  let password = m('input#password', {
-    type: 'text',
-    onkeypress: (e) => e.key == 'Enter' ? encrypt() : null,
-  })
-  let payload = m('textarea#payload')
-  let output = m('textarea#output')
-
   const encrypt = () => {
     output.dom.value = window.location.href.split('#!')[0] + '#!/decrypt/' + btoa(x.encrypt(password.dom.value, payload.dom.value))
     output.dom.parentNode.classList.remove('hidden')
     copyLink.dom.classList.remove('hidden')
   }
+
+  let password = m('input#password', {
+    type: 'text',
+    onkeypress: (e) => e.key == 'Enter' ? encrypt() : encryptMessage.dom.parentNode.classList.remove('hidden'),
+  })
+  let payload = m('textarea#payload', {
+    onkeypress: (e) => password.dom.parentNode.classList.remove('hidden'),
+  })
+  let output = m('textarea#output')
+
+  let encryptMessage = m('a.button.greenBg.whiteText', { onclick: encrypt }, 'Encrypt')
 
   const copy = () => {
     output.dom.select()
@@ -32,18 +36,16 @@ export default () => {
     view: () => ([
       m('div.container.center', null,
         m('div.field',
-          m('label', null, 'Message'), payload,
-        ),
-        m('div.field',
-          m('label', null, 'Create a Password'), password,
-        ),
-        m('div.field',
-          m('a.button.greenBg.whiteText', {
-            onclick: encrypt
-          }, 'Encrypt'),
+          m('label', null, 'Type your message'), payload,
         ),
         m('div.field.hidden',
-          m('label', null, 'Link'), output,
+          m('label', null, 'Create a Password'), password,
+        ),
+        m('div.field.hidden',
+          encryptMessage,
+        ),
+        m('div.field.hidden',
+          m('label', null, 'Share the link'), output,
         ),
         copyLink,
       ),
